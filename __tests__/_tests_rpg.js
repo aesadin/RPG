70ba1f2178299f76d2c81a33ff5619a09344ec9f
Allison Sadin <aesadin@gmail.com>
2020-06-24 14:12:22 -0700

    made our first test pass and wrote dog constructor
    Co-authored-by: Christine Augustine <blacchoy@gmail.com>
    Co-authored by: Kelly Eidsvik <kelly.eidsvik@gmail.com>

--- __tests__/_tests_rpg.js
+++ __tests__/_tests_rpg.js
@@ -3,11 +3,12 @@ import { Dog } from '../src/rpg.js';
 describe('Dog', () => {
 
   test('should create new dog player with 5 properties', () => {
-    const dog = new Dog("Tookie", "69hp", "420", "Barbarian", "Terrier");
+    const dog = new Dog("Tookie", "100hp", "1", "Barbarian", ["Terrier"]);
     expect(dog.name).toBe("Tookie");
-    expect(dog.health).toBe("69hp");
-    expect(dog.level).toBe("420");
-    expect(dog.class).toBe("Barbarian");
-    expect(dog.breed).toBe("Terrier");
+    expect(dog.health).toBe("100hp");
+    expect(dog.level).toBe("1");
+    expect(dog.charClass).toBe("Barbarian");
+    expect(dog.breed[0]).toBe("Terrier");
   });
 });
+
