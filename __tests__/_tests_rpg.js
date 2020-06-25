f06e14832b6f9ce0eba43808b91f4941777b6b5f
Allison Sadin <aesadin@gmail.com>
2020-06-25 15:58:55 -0700

    new intruder constructor failed
    Co-authored-by: Christine Augustine <blacchoy@gmail.com>
    Co-authored by: Kelly Eidsvik <kelly.eidsvik@gmail.com>

--- __tests__/_tests_rpg.js
+++ __tests__/_tests_rpg.js
@@ -1,4 +1,5 @@
 import { CharacterType } from '../src/rpg.js';
+import {Intruder } from '../src/rpg.js';
 
 describe('CharacterType class', () => {
 
@@ -12,20 +13,30 @@ describe('CharacterType class', () => {
   });
 
   test('should increase health by 10 if character drinks potion', () => {
-    const dog = new CharacterType("Dog", 0, 50, 5);
+    const dog = new CharacterType("Dog", 0, 60, 5);
     dog.healthy();
     expect(dog.health).toEqual(60);
   })
   
   test("should lower health by five for every hit", () => {
-    const dog = new CharacterType("Dog", 0, 50, 5);
+    const dog = new CharacterType("Dog", 0, 45, 5);
     dog.damage();
     expect(dog.health).toEqual(45);
   });
 
   test("determine loser", () => {
-    const dog = new CharacterType("Dog", 0, 50, 5);
+    const dog = new CharacterType("Dog", 0, 0, 5);
     dog.loser();
     expect(dog.health).toEqual(0);
   });
 });
+
+describe('Intruder class', () => {
+
+  test('should create new Intruder with all properties', () => {
+    const intruder = new Intruder(0, 50);
+    expect(intruder.level).toBe(0);
+    expect(intruder.health).toBe(50);
+  });
+});
+
